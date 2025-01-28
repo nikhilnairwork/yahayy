f9c8bf4670e245bfe6105297706e6d8a62073fcb
nikhilnairwork <nikhilnairwork27@gmail.com>
2025-01-28 10:28:16 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,9 +25,8 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center bg-indigo-700  justify-center bg-foreground text-background gap-2 hover:bg-[#983eff] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="/brands"
             rel="noopener noreferrer"
           >
             <Image
@@ -37,7 +36,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+          Brands
           </a>
           <a
             className="rounded-full bg-blue-700 border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
